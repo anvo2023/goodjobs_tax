49ace46e6b9f888587756db9c36678a96b26d43a
RobD01 <medbz2k3@gmail.com>
2023-06-16 22:36:53 -0700

    social links: changed to object data type

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,20 +1,26 @@
 const SocialComponent = () => {
   const socialLink = [
-    [
-      "https://cdn-icons-png.flaticon.com/512/1384/1384060.png",
-      "https://www.youtube.com/watch?v=aWIRSRMmdN0",
-    ],
-    [
-      "https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-icon-png-image_6315974.png",
-      "https://www.instagram.com/xuanloanfabrics/",
-    ],
+    {
+      image: "https://cdn-icons-png.flaticon.com/512/1384/1384060.png",
+      link: "https://www.youtube.com",
+    },
+    {
+      image:
+        "https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-icon-png-image_6315974.png",
+      link: "https://www.instagram.com",
+    },
   ];
 
   return (
     <div class="d-flex justify-content-center">
       {socialLink.map((item) => (
-        <a href={item[1]} target="_blank" rel="noreferrer" className="flexItem">
-          <img src={item[0]} alt={item} width="100%" />
+        <a
+          href={item.link}
+          target="_blank"
+          rel="noreferrer"
+          className="flexItem"
+        >
+          <img src={item.image} alt={item} width="100%" />
         </a>
       ))}
     </div>
